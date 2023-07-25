6b192486e14fb51e70ec343691a0abb8daef58af
jcabs13 <jcabshear@gmail.com>
2023-07-25 15:14:09 -0600

    just the app post

--- app.js
+++ app.js
@@ -5,71 +5,58 @@ const app = express();
 
 app.use(express.json());
 
-// Function to get Kroger API token
-const getKrogerToken = async () => {
-  // ... (same as before)
-};
+app.post('/getKrogerToken', (req, res) => {
+  console.log('Received POST from Glide');
 
-// Endpoint for /getkrogertoken
-app.post('/getkrogertoken', async (req, res) => {
-  console.log('Received POST for /getkrogertoken');
+  // Log the request body
+  console.log('Request Body:', req.body);
 
-  try {
-    // Get the Kroger API token
-    const krogerToken = await getKrogerToken();
-    console.log('Kroger API Token:', krogerToken);
+  const rowID = req.body.params.rowID.value;
+  const text = req.body.params.zip.value;
 
-    // Push the Kroger API token back to Glide
-    const rowID = req.body.params.rowID.value;
-    const token = process.env.BEARER_TOKEN;
-
-    axios({
-      method: 'post',
-      url: 'https://api.glideapp.io/api/function/mutateTables',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
-      },
-      data: {
-        "appID": "mtVYx3j3ot4FzRCdp3q4",
-        "mutations": [
-          {
-            "kind": "set-columns-in-row",
-            "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
-            "columnValues": {
-              "NqLF1": krogerToken
-            },
-            "rowID": rowID
-          }
-        ]
-      }
-    }).then((response) => {
-      console.log(response.data);
-      // Return the Kroger API token as a response
-      return res.status(200).json({ token: krogerToken });
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-      return res.sendStatus(500);
-    });
-
-  } catch (error) {
-    console.error('Error fetching Kroger API token:', error);
-    return res.sendStatus(500);
+  if (!rowID || !text) {
+    console.error('rowID or text not provided');
+    return res.sendStatus(400);
   }
-});
 
-// Existing endpoint for /webhook (unchanged)
-app.post('/webhook', async (req, res) => {
-  // ... (your existing /webhook logic here)
+  const token = process.env.BEARER_TOKEN;
+
+  axios({
+    method: 'post',
+    url: 'https://api.glideapp.io/api/function/mutateTables',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    data: {
+      "appID": "mtVYx3j3ot4FzRCdp3q4",
+      "mutations": [
+        {
+          "kind": "set-columns-in-row",
+          "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
+          "columnValues": {
+            "NqLF1": text
+          },
+          "rowID": rowID
+        }
+      ]
+    }
+  }).then((response) => {
+    console.log(response.data);
+    res.sendStatus(200);
+  }).catch((error) => {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+    res.sendStatus(500);
+  });
 });
 
 const port = process.env.PORT || 3000;
