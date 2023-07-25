71e5d298e195d2718b78f5ce18b8427ecf3fcead
jcabs13 <jcabshear@gmail.com>
2023-07-25 16:03:58 -0600

    fixed async

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//stable glide get and push
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
@@ -6,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-const getKrogerToken = async () => {
+const getKrogerToken = () => {
   const clientId = process.env.CLIENT_ID;
   const clientSecret = process.env.CLIENT_SECRET;
   const scope = process.env.SCOPE; // Adjust as per your requirements
@@ -14,19 +13,17 @@ const getKrogerToken = async () => {
   // Base64 encoding of clientId:clientSecret
   const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
 
-  try {
-    const response = await axios.post('https://api.kroger.com/v1/connect/oauth2/token', 'grant_type=client_credentials&scope=' + scope, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + credentials
-      },
+  return axios.post('https://api.kroger.com/v1/connect/oauth2/token', 'grant_type=client_credentials&scope=' + scope, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + credentials
+    },
+  })
+    .then(response => response.data.access_token)
+    .catch(error => {
+      console.error('Error fetching Kroger API token:', error);
+      throw error;
     });
-
-    console.log(response.data);
-    return response.data.access_token;
-  } catch (error) {
-    console.error('Error fetching Kroger API token:', error);
-  }
 };
 
 app.post('/getKrogerToken', (req, res) => {
@@ -37,51 +34,55 @@ app.post('/getKrogerToken', (req, res) => {
 
   const rowID = req.body.params.rowID?.value;
   const zip = req.body.params.zip?.value;
-  const krogerToken = await getKrogerToken();
 
   if (!rowID || !zip) {
     console.error('rowID or zip not provided');
     return res.sendStatus(400);
   }
 
-  const token = process.env.BEARER_TOKEN;
+  getKrogerToken()
+    .then(krogerToken => {
+      const token = process.env.BEARER_TOKEN;
 
-  axios({
-    method: 'post',
-    url: 'https://api.glideapp.io/api/function/mutateTables',
-    headers: { 
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
-    data: {
-      "appID": "mtVYx3j3ot4FzRCdp3q4",
-      "mutations": [
-        {
-          "kind": "set-columns-in-row",
-          "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
-          "columnValues": {
-            "NqLF1": krogerToken
-          },
-          "rowID": rowID
+      return axios({
+        method: 'post',
+        url: 'https://api.glideapp.io/api/function/mutateTables',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        data: {
+          "appID": "mtVYx3j3ot4FzRCdp3q4",
+          "mutations": [
+            {
+              "kind": "set-columns-in-row",
+              "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
+              "columnValues": {
+                "NqLF1": krogerToken
+              },
+              "rowID": rowID
+            }
+          ]
         }
-      ]
-    }
-  }).then((response) => {
-    console.log(response.data);
-    res.sendStatus(200);
-  }).catch((error) => {
-    if (error.response) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-    res.sendStatus(500);
-  });
+      });
+    })
+    .then(response => {
+      console.log(response.data);
+      res.sendStatus(200);
+    })
+    .catch(error => {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+      res.sendStatus(500);
+    });
 });
 
 const port = process.env.PORT || 3000;
