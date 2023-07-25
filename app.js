6670056a5795e6218ca8cd337f62e5b71f467c14
jcabs13 <jcabshear@gmail.com>
2023-07-25 15:10:13 -0600

    only the /getkrogertoken endpoint

--- app.js
+++ app.js
@@ -1,77 +1,75 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
 app.use(express.json());
 
+// Function to get Kroger API token
 const getKrogerToken = async () => {
-  const clientId = process.env.CLIENT_ID;
-  const clientSecret = process.env.CLIENT_SECRET;
-  const scope = process.env.SCOPE;
+  // ... (same as before)
+};
 
-  const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+// Endpoint for /getkrogertoken
+app.post('/getkrogertoken', async (req, res) => {
+  console.log('Received POST for /getkrogertoken');
 
   try {
-    const response = await axios.post('https://api.kroger.com/v1/connect/oauth2/token', 'grant_type=client_credentials&scope=' + scope, {
+    // Get the Kroger API token
+    const krogerToken = await getKrogerToken();
+    console.log('Kroger API Token:', krogerToken);
+
+    // Push the Kroger API token back to Glide
+    const rowID = req.body.params.rowID.value;
+    const token = process.env.BEARER_TOKEN;
+
+    axios({
+      method: 'post',
+      url: 'https://api.glideapp.io/api/function/mutateTables',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + credentials
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
       },
+      data: {
+        "appID": "mtVYx3j3ot4FzRCdp3q4",
+        "mutations": [
+          {
+            "kind": "set-columns-in-row",
+            "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
+            "columnValues": {
+              "NqLF1": krogerToken
+            },
+            "rowID": rowID
+          }
+        ]
+      }
+    }).then((response) => {
+      console.log(response.data);
+      // Return the Kroger API token as a response
+      return res.status(200).json({ token: krogerToken });
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+      return res.sendStatus(500);
     });
 
-    return response.data.access_token;
   } catch (error) {
     console.error('Error fetching Kroger API token:', error);
-    throw error;
-  }
-};
-
-const getKrogerLocations = async (zip, krogerToken) => {
-  try {
-    const response = await axios.get(`https://api.kroger.com/v1/locations?filter.zipCode.near=${zip}`, {
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${krogerToken}`
-      },
-    });
-
-    const locations = response.data.data.slice(0, 3).map(location => location.location.address.addressLine1);
-    const locationsString = locations.join(', ');
-
-    return locationsString;
-  } catch (error) {
-    console.error('Error fetching Kroger locations:', error);
-    throw error;
-  }
-};
-
-app.post('/getKrogerToken', async (req, res) => {
-  console.log('Received POST from Glide for getKrogerToken');
-
-  try {
-    const krogerToken = await getKrogerToken();
-    console.log('Kroger Token:', krogerToken);
-    res.json({ krogerToken });
-  } catch (error) {
-    console.error('Error in getKrogerToken:', error);
-    res.status(500).send({ error: 'Error fetching Kroger API token.' });
+    return res.sendStatus(500);
   }
 });
 
-app.post('/getKrogerLocations', async (req, res) => {
-  console.log('Received POST from Glide for getKrogerLocations');
-
-  const zip = req.body.params.zip.value;
-  const krogerToken = req.body.params.krogerToken.value;
-
-  try {
-    const locationsString = await getKrogerLocations(zip, krogerToken);
-    console.log('Locations String:', locationsString);
-    res.json({ locations: locationsString });
-  } catch (error) {
-    console.error('Error in getKrogerLocations:', error);
-    res.status(500).send({ error: 'Error fetching Kroger locations.' });
-  }
+// Existing endpoint for /webhook (unchanged)
+app.post('/webhook', async (req, res) => {
+  // ... (your existing /webhook logic here)
 });
 
 const port = process.env.PORT || 3000;
