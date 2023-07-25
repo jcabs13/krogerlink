b2354d66e532ad1a91b41a27f2d349cddf505e01
jcabs13 <jcabshear@gmail.com>
2023-07-25 15:38:42 -0600

    last night with new endpoint

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// require and configure dotenv if you haven't done it already
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
@@ -6,17 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-// New /getKrogerToken endpoint
-app.get('/getKrogerToken', (req, res) => {
-  // Add any logic here to fetch the Kroger token
-  const krogerToken = 'YOUR_KROGER_TOKEN'; // Replace with your actual logic to get the token
-
-  // Return the Kroger token as a response
-  res.status(200).json({ token: krogerToken });
-});
-
-// Existing /webhook endpoint moved here
-app.post('/webhook', (req, res) => {
+app.post('/getKrogerToken', (req, res) => {
   console.log('Received POST from Glide');
 
   // Log the request body
@@ -30,52 +19,44 @@ app.post('/webhook', (req, res) => {
     return res.sendStatus(400);
   }
 
-  // Use the /getKrogerToken endpoint to fetch the Kroger token
-  axios.get('http://localhost:3000/getKrogerToken')
-    .then((response) => {
-      const token = response.data.token;
+  const token = process.env.BEARER_TOKEN;
 
-      axios({
-        method: 'post',
-        url: 'https://api.glideapp.io/api/function/mutateTables',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        data: {
-          "appID": "mtVYx3j3ot4FzRCdp3q4",
-          "mutations": [
-            {
-              "kind": "set-columns-in-row",
-              "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
-              "columnValues": {
-                "NqLF1": text
-              },
-              "rowID": rowID
-            }
-          ]
+  axios({
+    method: 'post',
+    url: 'https://api.glideapp.io/api/function/mutateTables',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    data: {
+      "appID": "mtVYx3j3ot4FzRCdp3q4",
+      "mutations": [
+        {
+          "kind": "set-columns-in-row",
+          "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
+          "columnValues": {
+            "NqLF1": text
+          },
+          "rowID": rowID
         }
-      }).then((response) => {
-        console.log(response.data);
-        res.sendStatus(200);
-      }).catch((error) => {
-        if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          console.log(error.request);
-        } else {
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
-        res.sendStatus(500);
-      });
-    })
-    .catch((error) => {
-      console.log('Error fetching Kroger token:', error.message);
-      res.sendStatus(500);
-    });
+      ]
+    }
+  }).then((response) => {
+    console.log(response.data);
+    res.sendStatus(200);
+  }).catch((error) => {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+    res.sendStatus(500);
+  });
 });
 
 const port = process.env.PORT || 3000;
