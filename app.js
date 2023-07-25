05ba44b3681d31ed171c91ea8f66c95afb63430f
jcabs13 <jcabshear@gmail.com>
2023-07-24 21:20:34 -0600

    add webhook get

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
 require('dotenv').config();
+const express = require('express');
 const axios = require('axios');
+const app = express();
 
-const token = process.env.BEARER_TOKEN;
+app.use(express.json());
 
-axios({
-  method: 'post',
-  url: 'https://api.glideapp.io/api/function/mutateTables',
-  headers: { 
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  },
-  data: {
-    "appID": "mtVYx3j3ot4FzRCdp3q4",
-    "mutations": [
-      {
-        "kind": "set-columns-in-row",
-        "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
-        "columnValues": {
-          "NqLF1": "Received from webhook"
-        },
-        "rowID": "0f3MGHVYQTSHn4FwKReDZw" // replace 'ROW-ID' with your actual row id
-      }
-    ]
-  }
-}).then((response) => {
-  console.log(response.data);
-}).catch((error) => {
-  if (error.response) {
-    console.log(error.response.data);
-    console.log(error.response.status);
-    console.log(error.response.headers);
-  } else if (error.request) {
-    console.log(error.request);
-  } else {
-    console.log('Error', error.message);
-  }
-  console.log(error.config);
+app.post('/webhook', (req, res) => {
+  console.log('Received POST from Glide');
+  
+  const token = process.env.BEARER_TOKEN;
+
+  axios({
+    method: 'post',
+    url: 'https://api.glideapp.io/api/function/mutateTables',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    data: {
+      "appID": "mtVYx3j3ot4FzRCdp3q4",
+      "mutations": [
+        {
+          "kind": "set-columns-in-row",
+          "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
+          "columnValues": {
+            // ... your columnValues here ...
+          },
+          "rowID": "ROW-ID" // replace 'ROW-ID' with your actual row id
+        }
+      ]
+    }
+  }).then((response) => {
+    console.log(response.data);
+    res.sendStatus(200);
+  }).catch((error) => {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+    res.sendStatus(500);
+  });
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
