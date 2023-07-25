cf3b9749b640e6c9484f3e7d2312a77381bea2e9
jcabs13 <jcabshear@gmail.com>
2023-07-24 21:54:36 -0600

    added error for missing glide variable

--- app.js
+++ app.js
@@ -1,50 +1,60 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
 app.post('/webhook', (req, res) => {
-    console.log('Received POST from Glide');
-    
-    const token = process.env.BEARER_TOKEN;
+  console.log('Received POST from Glide');
   
-    // Extract rowID and text from incoming request
-    const glideRowId = req.body.rowID;
-    const glideText = req.body.text; // replace 'text' with the actual property name if different
+  const token = process.env.BEARER_TOKEN;
+
+  const rowID = req.body?.rowID;
+  const text = req.body?.text;
   
-    axios({
-      method: 'post',
-      url: 'https://api.glideapp.io/api/function/mutateTables',
-      headers: { 
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
-      },
-      data: {
-        "appID": "mtVYx3j3ot4FzRCdp3q4",
-        "mutations": [
-          {
+  if (!rowID || !text) {
+    console.error('rowID or text not provided');
+    return res.sendStatus(400);
+  }
+
+  axios({
+    method: 'post',
+    url: 'https://api.glideapp.io/api/function/mutateTables',
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    data: {
+      "appID": "mtVYx3j3ot4FzRCdp3q4",
+      "mutations": [
+        {
             "kind": "set-columns-in-row",
             "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
             "columnValues": {
-              "NqLF1": glideText // Use the text from the request
+              "NqLF1": text
             },
-            "rowID": glideRowId 
-          }
-        ]
-      }
-    }).then((response) => {
-      console.log(response.data);
-      res.sendStatus(200);
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-      res.sendStatus(500);
-    });
+          "rowID": rowID
+        }
+      ]
+    }
+  }).then((response) => {
+    console.log(response.data);
+    res.sendStatus(200);
+  }).catch((error) => {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+    res.sendStatus(500);
   });
-  
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Server listening on port ${port}`));
-  
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
