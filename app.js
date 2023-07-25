c0c7a8f3ed6fa049e9fa370bf1e2cfb4632aa8a0
jcabs13 <jcabshear@gmail.com>
2023-07-25 15:32:51 -0600

    back to last night

--- app.js
+++ app.js
@@ -5,37 +5,14 @@ const app = express();
 
 app.use(express.json());
 
-const getKrogerToken = async () => {
-  const clientId = process.env.CLIENT_ID;
-  const clientSecret = process.env.CLIENT_SECRET;
-  const scope = process.env.SCOPE; // Adjust as per your requirements
-
-  // Base64 encoding of clientId:clientSecret
-  const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
-
-  try {
-    const response = await axios.post('https://api.kroger.com/v1/connect/oauth2/token', 'grant_type=client_credentials&scope=' + scope, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + credentials
-      },
-    });
-
-    console.log(response.data);
-    return response.data.access_token;
-  } catch (error) {
-    console.error('Error fetching Kroger API token:', error);
-  }
-};
-
-app.post('/webhook', async (req, res) => {
+app.post('/webhook', (req, res) => {
   console.log('Received POST from Glide');
 
   // Log the request body
   console.log('Request Body:', req.body);
 
   const rowID = req.body.params.rowID.value;
-  const text = req.body.params.zip.value;
+  const text = req.body.params.text.value;
 
   if (!rowID || !text) {
     console.error('rowID or text not provided');
@@ -43,10 +20,6 @@ app.post('/webhook', async (req, res) => {
   }
 
   const token = process.env.BEARER_TOKEN;
-  const krogerToken = await getKrogerToken();
-
-  // Now you can use krogerToken with the Kroger API.
-  console.log('Kroger token:', krogerToken);
 
   axios({
     method: 'post',
