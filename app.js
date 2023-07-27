9086426000e805e91c47f511bb7cb8bc24aff5ab
jcabs13 <jcabshear@gmail.com>
2023-07-27 14:29:57 -0600

    back to stable

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//everything works
+//token and location webhooks working
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
@@ -219,129 +219,79 @@ const getAisle = async (term, locID, token) => {
 
   if (data && Array.isArray(data.data)) {
     let aisle = data.data[0]?.aisleLocations[0]?.description; // get the description of the first location
-    let category = data.data[0]?.categories[0]?.description;
-    let image = data.data[0]?.images[0]?.description;
-
 
     console.log('Returning Aisle Location:', aisle);
-    console.log('All Item Data:', data);
 
     return aisle;
-    return category;
-    return image;
   } else {
     console.error('Invalid data structure from Kroger:', data);
     return null;
   }
 };
 
-app.post('/getAisle', async (req, res) => {
+app.post('/getAisle', (req, res) => {
   console.log('Received POST from Glide');
 
   // Log the request body
   console.log('Request Body:', req.body);
 
-  let terms = req.body.params.terms?.value; 
+  const term = req.body.params.term?.value;
   const locID = req.body.params.locID?.value;
   const token = req.body.params.token?.value;
   const rowID = req.body.params.rowID?.value;
 
-  // Convert the string into an array
-  terms = terms.split('///');
-
-  console.log('INPUT terms:', terms);
+  console.log('INPUT term:', term);
   console.log('INPUT locID:', locID);
 
-  if (!terms || terms.length === 0 || !locID) {
-    console.error('terms or locationID not provided');
+  if (!term || !locID) {
+    console.error('term of locationID not provided');
     return res.sendStatus(400);
   }
 
-  let aisles = [];
-  for (const term of terms) {
-    try {
-      let aisle = await getAisle(term, locID, token);
-      aisles.push(aisle);
-    } catch (error) {
-      console.error('Error getting aisle for term:', term, error);
-      res.sendStatus(500);
-      return;
-    }
-  }
+  getAisle(term, locID, token)
+    .then(aisle => {
+      const token = process.env.BEARER_TOKEN;
 
-  let categories = [];
-  for (const term of terms) {
-    try {
-      let category = await getAisle(term, locID, token);
-      categories.push(category);
-    } catch (error) {
-      console.error('Error getting category for term:', term, error);
-      res.sendStatus(500);
-      return;
-    }
-  }
-
-  let images = [];
-  for (const term of terms) {
-    try {
-      let image = await getAisle(term, locID, token);
-      images.push(image);
-    } catch (error) {
-      console.error('Error getting image for term:', term, error);
-      res.sendStatus(500);
-      return;
-    }
-  }
-
-  // Convert the array into a string
-  aisles = aisles.join('///');
-
-  const bearerToken = process.env.BEARER_TOKEN;
-
-  axios({
-    method: 'post',
-    url: 'https://api.glideapp.io/api/function/mutateTables',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${bearerToken}`,
-    },
-    data: {
-      "appID": "mtVYx3j3ot4FzRCdp3q4",
-      "mutations": [
-        {
-          "kind": "set-columns-in-row",
-          "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
-          "columnValues": {
-            "HenO1": aisles,
-            "5vQzp": categories,
-            "CNtmj": images
-          },
-          "rowID": rowID
+      return axios({
+        method: 'post',
+        url: 'https://api.glideapp.io/api/function/mutateTables',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        data: {
+          "appID": "mtVYx3j3ot4FzRCdp3q4",
+          "mutations": [
+            {
+              "kind": "set-columns-in-row",
+              "tableName": "native-table-MX8xNW5WWoJhW4fwEeN7",
+              "columnValues": {
+                "HenO1": aisle
+              },
+              "rowID": rowID
+            }
+          ]
         }
-      ]
-    }
-  })
-  .then(response => {
-    console.log(response.data);
-    res.sendStatus(200);
-  })
-  .catch(error => {
-    if (error.response) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-    res.sendStatus(500);
-  });
+      });
+    })
+    .then(response => {
+      console.log(response.data);
+      res.sendStatus(200);
+    })
+    .catch(error => {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+      res.sendStatus(500);
+    });
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
